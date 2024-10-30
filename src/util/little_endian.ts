4487bf2fa18ff367bbaf1cc19f816f2ff356d6ca
redsolver <info@redsolver.net>
2024-10-31 00:02:12 +0100

    add src/util/little_endian.ts

---
+++ src/util/little_endian.ts
@@ -0,0 +1,16 @@
+function encodeLittleEndian(value: number, length: number): Uint8Array {
+    const buffer = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+        buffer[i] = value & 0xFF;
+        value = value >> 8;
+    }
+    return buffer;
+}
+
+function decodeLittleEndian(bytes: Uint8Array): number {
+    let total = 0;
+    for (let i = 0; i < bytes.length; i++) {
+        total += bytes[i] * Math.pow(256, i);
+    }
+    return total;
+}
