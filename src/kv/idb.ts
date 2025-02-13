6d658adb7c38e85a0ee87bf325b5ca3998935c58
redsolver <red@s5.pro>
2025-02-13 14:05:58 +0100

    fix(kv/idb): create object store before using it

--- src/kv/idb.ts
+++ src/kv/idb.ts
@@ -3,7 +3,11 @@ import { KeyValueStore } from "./kv";
 
 export class IDBStore implements KeyValueStore {
     static async open(name: string): Promise<IDBStore> {
-        const db = await openDB<Uint8Array>(name);
+        const db = await openDB<Uint8Array>(name, 1, {
+            upgrade(db) {
+                db.createObjectStore('kv');
+            },
+        });
         return new IDBStore(db);
     }
     private readonly db: IDBPDatabase<Uint8Array>;
@@ -13,10 +17,10 @@ export class IDBStore implements KeyValueStore {
     }
 
     async put(key: Uint8Array, value: Uint8Array): Promise<void> {
-        await this.db.put("data", value, key);
+        await this.db.put("kv", value, key);
     }
     async get(key: Uint8Array): Promise<Uint8Array | undefined> {
-        return await this.db.get("data", key);
+        return await this.db.get("kv", key);
     }
     async contains(key: Uint8Array): Promise<boolean> {
         return (await this.get(key)) !== undefined;
