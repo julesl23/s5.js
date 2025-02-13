cc9422a1b41b86546b66cef41d3f15c126cf1531
redsolver <red@s5.pro>
2025-02-13 13:58:48 +0100

    fix(util): trim multibase prefix from base64url encoding

--- src/util/base64.ts
+++ src/util/base64.ts
@@ -1,9 +1,9 @@
 import { base64url } from "multiformats/bases/base64";
 
 export function base64UrlNoPaddingEncode(input: Uint8Array): string {
-    return base64url.encode(input);
+    return base64url.encode(input).substring(1);
 }
 
 export function base64UrlNoPaddingDecode(input: string): Uint8Array {
-    return base64url.decode(input);
+    return base64url.decode('u' + input);
 }
