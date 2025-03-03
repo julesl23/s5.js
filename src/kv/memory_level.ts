066d77323193739fbfe2c67cb02edd411235cf6c
redsolver <red@s5.pro>
2025-03-03 04:15:10 +0100

    feat(kv): add MemoryLevelStore

---
+++ src/kv/memory_level.ts
@@ -0,0 +1,26 @@
+import { MemoryLevel } from "memory-level";
+import { KeyValueStore } from "./kv";
+
+export class MemoryLevelStore implements KeyValueStore {
+    static async open(): Promise<MemoryLevelStore> {
+        const db = new MemoryLevel<Uint8Array, Uint8Array>({
+            keyEncoding: 'view',
+            valueEncoding: 'view',
+            storeEncoding: 'view',
+        })
+        return new MemoryLevelStore(db);
+    }
+    private readonly db: MemoryLevel<Uint8Array, Uint8Array>;
+    constructor(db: MemoryLevel<Uint8Array, Uint8Array>) {
+        this.db = db;
+    }
+    async put(key: Uint8Array, value: Uint8Array): Promise<void> {
+        await this.db.put(key, value);
+    }
+    async get(key: Uint8Array): Promise<Uint8Array | undefined> {
+        return await this.db.get(key);
+    }
+    async contains(key: Uint8Array): Promise<boolean> {
+        return await this.db.has(key);
+    }
+}
