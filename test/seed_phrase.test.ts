6b7a3eca834134c6f9f2a1468f0bd924c4e1057e
redsolver <red@s5.pro>
2025-03-11 21:14:05 +0100

    test: add seed_phrase test

---
+++ test/seed_phrase.test.ts
@@ -0,0 +1,26 @@
+import { expect, test, describe } from "bun:test";
+import { JSCryptoImplementation } from "../src/api/crypto/js";
+import { generatePhrase, hashToChecksumWords, validatePhrase } from "../src/identity/seed_phrase/seed_phrase";
+import { bytesToHex } from "@noble/hashes/utils";
+
+describe("seed_phrase", () => {
+    const crypto = new JSCryptoImplementation();
+    test("generatePhrase", () => {
+        let phrase1 = generatePhrase(crypto);
+        let phrase2 = generatePhrase(crypto);
+        expect(phrase1).not.toBe(phrase2);
+        expect(phrase1.split(" ").length).toBe(15);
+    });
+    test("validatePhrase", () => {
+        const [valid, error, seed] = validatePhrase('sign weight spy quality sudden minute pizza great fear adapt nobody evident control alive crime', crypto);
+        expect(valid).toBe(true);
+        expect(error).toBe("");
+        expect(bytesToHex(seed!)).toBe("cafe1d4accd7636ab5865140e97d2eb8");
+    });
+    test("hashToChecksumWords", () => {
+        const hash = crypto.hashBlake3Sync(new Uint8Array(0));
+        const checksumWords = hashToChecksumWords(hash);
+        expect(checksumWords[0]).toBe(700);
+        expect(checksumWords[1]).toBe(308);
+    });
+});
