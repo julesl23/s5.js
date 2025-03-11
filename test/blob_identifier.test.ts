92553491103b6126c5f8ab186799837a969ae1c9
redsolver <red@s5.pro>
2025-03-11 21:05:09 +0100

    test: add tests for blob_identifier and utils

---
+++ test/blob_identifier.test.ts
@@ -0,0 +1,67 @@
+import { expect, test, describe } from "bun:test";
+import { BlobIdentifier } from "../src/identifier/blob";
+import { bytesToHex, hexToBytes } from "@noble/hashes/utils";
+
+describe("blob_identifier", () => {
+  describe("blob sizes", () => {
+    const prefix = 'blobaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+    const testCases = new Map()
+    testCases.set(0, 'aa',);
+    testCases.set(1, 'ab',);
+    testCases.set(42, 'bk',);
+    testCases.set(255, 'h7',);
+    testCases.set(256, 'aaae',);
+    testCases.set(65535, 'h774',);
+    testCases.set(65536, 'aaaaaq',);
+    testCases.set(57124823, 'gxu5tqg');
+
+    for (let [size, expected] of testCases) {
+      test(`size ${size}`, () => {
+        const blobIdentifier = new BlobIdentifier(
+          new Uint8Array(32),
+          size,
+        );
+        expect(blobIdentifier.toBase32()).toBe(prefix + expected);
+      });
+    }
+  });
+  describe("encoding", () => {
+    const hashHex = '1ee40b877081c208036d451b898700b4bf938792ffa3e4426f2bf871aa057d992b';
+    const hash = hexToBytes(hashHex);
+    const size = 5050;
+    const blobId = new BlobIdentifier(hash, size);
+
+    test('hex', () => {
+      const hexBlobId = 'f5b821ee40b877081c208036d451b898700b4bf938792ffa3e4426f2bf871aa057d992bba13';
+      expect(blobId.toHex())
+        .toBe(hexBlobId);
+      const decoded = BlobIdentifier.decode(hexBlobId);
+      expect(bytesToHex(decoded.hash)).toBe(hashHex);
+      expect(decoded.size).toBe(size);
+    });
+    test('base32', () => {
+      const base32BlobId = 'blobb5zalq5yidqqianwukg4jq4aljp4tq6jp7i7eijxsx6drvicx3gjlxijq';
+      expect(blobId.toBase32())
+        .toBe(base32BlobId);
+      const decoded = BlobIdentifier.decode(base32BlobId);
+      expect(bytesToHex(decoded.hash)).toBe(hashHex);
+      expect(decoded.size).toBe(size);
+    });
+    test('base58', () => {
+      const base58BlobId = 'z44t3p46N4Fmqc1STusbaEoZ2HeDCFNNFRGys8SJsDvV9kHyMUbt';
+      expect(blobId.toBase58())
+        .toBe(base58BlobId);
+      const decoded = BlobIdentifier.decode(base58BlobId);
+      expect(bytesToHex(decoded.hash)).toBe(hashHex);
+      expect(decoded.size).toBe(size);
+    });
+    test('base64url', () => {
+      const base64BlobId = 'uW4Ie5AuHcIHCCANtRRuJhwC0v5OHkv-j5EJvK_hxqgV9mSu6Ew';
+      expect(blobId.toBase64Url())
+        .toBe(base64BlobId);
+      const decoded = BlobIdentifier.decode(base64BlobId);
+      expect(bytesToHex(decoded.hash)).toBe(hashHex);
+      expect(decoded.size).toBe(size);
+    });
+  });
+});
---
+++ test/util.test.ts
@@ -0,0 +1,29 @@
+import { expect, test, describe } from "bun:test";
+import { deriveHashInt, deriveHashString } from "../src/util/derive_hash";
+import { JSCryptoImplementation } from "../src/api/crypto/js";
+import { bytesToHex } from "@noble/hashes/utils";
+
+describe("derive_hash", () => {
+  const crypto = new JSCryptoImplementation();
+  test("deriveHashString", () => {
+    let hash = deriveHashString(
+      new Uint8Array(32),
+      new Uint8Array(32),
+      crypto,
+    );
+    expect(bytesToHex(hash)).toBe(
+      "e40b877081c208036d451b898700b4bf938792ffa3e4426f2bf871aa057d992b",
+    );
+  });
+
+  test("deriveHashInt", () => {
+    let hash = deriveHashInt(
+      new Uint8Array(32),
+      0,
+      crypto,
+    );
+    expect(bytesToHex(hash)).toBe(
+      "4d006976636a8696d909a630a4081aad4d7c50f81afdee04020bf05086ab6a55",
+    );
+  });
+});
